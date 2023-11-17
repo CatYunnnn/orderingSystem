b1178744107a53f74cbc0ece165aabda16eb8cf5
CatYun <marco20001114@gmail.com>
2023-11-17 12:26:24 +0800

    modify seed file

--- server/models/seeds/menu.js
+++ server/models/seeds/menu.js
@@ -1,24 +1,64 @@
 const mysql = require("mysql2");
 const pool = require("../../config/mysql");
-////定義schema
-
-const limitedTimeOffer = `CREATE TABLE limitedTimeOffer(id
+const data = require("./seedData.json");
+////定義種子schema
+const seed = function (type) {
+  return `CREATE TABLE ${type}(id
   INT PRIMARY KEY AUTO_INCREMENT,
-  NAME VARCHAR(10),
+  NAME VARCHAR(20) CHARACTER SET utf8mb4,
   price INT,
-  type VARCHAR(10))`;
-
-////創建種子table
-pool.getConnection((err, connection) => {
-  if (err) console.error(err);
-  console.log("connected to the databases");
-
-  connection.query(limitedTimeOffer, (err, results) => {
+  type VARCHAR(20))`;
+};
+////插入種子資料
+const insert = function (type, name, price) {
+  return `INSERT INTO ${type} (NAME,price,type) VALUES ('${name}',${price},'${type}')`;
+};
+////不同種類的table
+const type = [
+  `limitedTimeOffer`,
+  `nigiriSushi`,
+  `gunkanMaki`,
+  `noodles`,
+  `sideDishes`,
+  `desserts`,
+];
+//創建種子table
+type.forEach((item) => {
+  pool.getConnection((err, connection) => {
     if (err) console.error(err);
-    else console.log("table created successfully");
+    console.log("connected to the databases");
+
+    connection.query(seed(item), (err, results) => {
+      if (err) console.error(err);
+      else console.log("table created successfully");
+    });
+
+    connection.release();
+
+    console.log("database connection released");
   });
-
-  connection.release();
-
-  console.log("database connection released");
 });
+//把種子資料輸入進資料庫
+
+for (let key in data) {
+  ////key = data.limitedTimeOffer...
+  for (let item in data[key]) {
+    ////item = data.limitedTimeOffer.北海道產秋刀魚...
+    pool.getConnection((err, connection) => {
+      if (err) console.error(err);
+      console.log("connected to the databases");
+
+      connection.query(
+        insert(`${key}`, `${item}`, data[key][item]),
+        (err, results) => {
+          if (err) console.error(err);
+          else console.log("data inserted successfully");
+        }
+      );
+
+      connection.release();
+
+      console.log("database connection released");
+    });
+  }
+}
