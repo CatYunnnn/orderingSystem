22e84df5f84e89604b2eef4bceffbb9c22d421d4
CatYun <marco20001114@gmail.com>
2023-11-19 15:39:51 +0800

    modify seed & add process exit

--- server/models/seeds/menu.js
+++ server/models/seeds/menu.js
@@ -9,7 +9,7 @@ const seed = function (type) {
   price INT,
   type VARCHAR(20))`;
 };
-////插入種子資料
+////種子插入格式
 const insert = function (type, name, price) {
   return `INSERT INTO ${type} (NAME,price,type) VALUES ('${name}',${price},'${type}')`;
 };
@@ -22,43 +22,57 @@ const type = [
   `sideDishes`,
   `desserts`,
 ];
-//創建種子table
-type.forEach((item) => {
-  pool.getConnection((err, connection) => {
-    if (err) console.error(err);
-    console.log("connected to the databases");
 
+////創建Promise
+//table 
+const creatSeedTable = function (connection, item) {
+  return new Promise((resolve, reject) => {
     connection.query(seed(item), (err, results) => {
-      if (err) console.error(err);
-      else console.log("table created successfully");
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log("table created successfully");
+        connection.release();
+        resolve();
+      }
     });
-
-    connection.release();
-
-    console.log("database connection released");
   });
-});
-//把種子資料輸入進資料庫
-
-for (let key in data) {
-  ////key = data.limitedTimeOffer...
-  for (let item in data[key]) {
-    ////item = data.limitedTimeOffer.北海道產秋刀魚...
+};
+//seeds
+const insertSeed = (conn, key, item, data) => {
+  return new Promise((resolve, reject) => {
+    conn.query(insert(`${key}`, `${item}`, data[key][item]), (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else console.log("data inserted successfully");
+      conn.release();
+      resolve();
+    });
+  });
+};
+////創建table並插入種子
+(async () => {
+  
+  let connection = await new Promise((resolve, reject) => {
     pool.getConnection((err, connection) => {
       if (err) console.error(err);
-      console.log("connected to the databases");
-
-      connection.query(
-        insert(`${key}`, `${item}`, data[key][item]),
-        (err, results) => {
-          if (err) console.error(err);
-          else console.log("data inserted successfully");
-        }
-      );
-
-      connection.release();
-
-      console.log("database connection released");
+      console.log(connection);
+      resolve(connection);
     });
+  });
+
+  for (let item of type) {
+    await creatSeedTable(connection, item);
   }
-}
+
+  for (let key in data) {
+    ////key = data.limitedTimeOffer...
+    for (let item in data[key]) {
+      ////item = data.limitedTimeOffer.北海道產秋刀魚...
+      await insertSeed(connection, key, item, data);
+    }
+  }
+  process.exit();
+})();
