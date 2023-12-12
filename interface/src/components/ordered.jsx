062d4100818e454bb448e1dc56ffa594ec4c05f4
CatYun <marco20001114@gmail.com>
2023-12-12 14:33:42 +0800

    finish ordered list

--- interface/src/components/ordered.jsx
+++ interface/src/components/ordered.jsx
@@ -2,26 +2,31 @@ import React from "react";
 import styles from "../styles/ordered.module.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+
 ////點餐明細
 const Ordered = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState({});
 
   useEffect(() => {
-    
     axios
       .get(`http://localhost:5000/page/ordered`)
       .then((res) => {
         setData(res.data);
-        console.log(res.data);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   }, []);
 
+  ////轉成陣列叫好用map
+  let items = Object.keys(data);
+
   return (
     <div>
-      <button className={styles.backToHome}>返回首頁</button>
+      <Link className={styles.link} to="/">
+        <button className={styles.backToHome}>返回首頁</button>
+      </Link>
       <p className={styles.remind}>這是您現在的點餐內容</p>
       <main>
         <div className={styles.leftSide}>
@@ -30,12 +35,17 @@ const Ordered = () => {
             <div className={styles.titleAmount}>數量</div>
             <div className={styles.titleFinish}>上菜</div>
           </div>
-          <div className={styles.recordRow}>
-            <div className={styles.id}>1</div>
-            <div className={styles.dishName}>天婦羅</div>
-            <div className={styles.amount}>2</div>
-            <div className={styles.finish}>結束</div>
-          </div>
+          {/*渲染下單的餐點*/}
+          {items.map((key) => {
+            return (
+              <div className={styles.recordRow}>
+                <div className={styles.id}>{Number(key) + 1}</div>
+                <div className={styles.dishName}>{data[key].name}</div>
+                <div className={styles.amount}>{data[key].amount}</div>
+                <div className={styles.finish}>結束</div>
+              </div>
+            );
+          })}
         </div>
         <div className={styles.mid}>
           <div className={styles.scrollButtonUp}>上</div>
